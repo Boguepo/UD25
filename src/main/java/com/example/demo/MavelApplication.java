437e3557543a471d6c3bb4accc5fd51b31cc3068
Boguepo <46709093+Boguepo@users.noreply.github.com>
2021-03-10 12:28:27 +0100

    Maven

---
+++ src/main/java/com/example/demo/MavelApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavelApplication.class, args);
+	}
+
+}
